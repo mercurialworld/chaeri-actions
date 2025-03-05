ed708a505de71fe98af9fe74a2a10602c4b318bc
ポッチャマ <mercworld@pocha.moe>
2025-03-04 19:36:43 -0500

    config: modularize rollup config

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,17 +1,15 @@
-// See: https://rollupjs.org/introduction/
-
 import commonjs from "@rollup/plugin-commonjs";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 
-const config = [
-    {
-        input: "deploy-codedeploy/index.ts",
+function createConfig(actionPath) {
+    return {
+        input: `${actionPath}/index.ts`,
         output: {
             compact: true,
             esModule: true,
-            file: "dist/deploy-codedeploy/index.js",
+            file: `dist/${actionPath}/index.js`,
             format: "es",
             inlineDynamicImports: true,
             sourcemap: false,
@@ -22,24 +20,11 @@ const config = [
             commonjs(),
             json(),
         ],
-    },
-    {
-        input: "parse-cdk/index.ts",
-        output: {
-            compact: true,
-            esModule: true,
-            file: "dist/parse-cdk/index.js",
-            format: "es",
-            inlineDynamicImports: true,
-            sourcemap: false,
-        },
-        plugins: [
-            typescript(),
-            nodeResolve({ preferBuiltins: true }),
-            commonjs(),
-            json(),
-        ],
-    },
-];
+    };
+}
+
+const actions = ["deploy-codedeploy", "parse-cdk"];
+
+const config = actions.map(createConfig);
 
 export default config;
